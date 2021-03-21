d2375e9907bafdca25254f6eedef2196b55dbef6
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2021-03-21 19:14:18 +0700

    variable api dan get token

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,16 +4,59 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+	private static $main_url = 'https://sandbox.bca.co.id';
+	private static $api_key = '6b356c47-0c9e-45d4-840b-f28b45f1cc28';
+	private static $api_secret = '98566c4d-bc3d-4485-9b64-9d674473eb46';
+	private static $client_id = '7eaf4e7b-b876-48ff-ae89-944369e1740a';
+	private static $client_secret = 'fc3d9282-ef06-4f68-83d9-ef18af8727c9';
+	private static $corporateid = 'BCAAPI2016';
+	private static $account_number = '0201245680';
+	private static $access_token    = null;
+	private static $timeStamp       = null;
+	
+
+	public function __construct()
+    {
+        parent::__construct();
+        $this->guzzle       = new \GuzzleHttp\Client;
+        self::$timeStamp    = date('o-m-d') . 'T' . date('H:i:s') . '.' . substr(date('u'), 0, 3) . date('P');
+		$this->getToken();
+    }
+
 	public function index()
 	{
+		var_dump(self::$access_token);
+		die;
 		// $this->load->view('template/header');
 		// $this->load->view('template/topbar');
 		// $this->load->view('template/sidebar');
 		// $this->load->view('page/home/index');
 		// $this->load->view('template/footer');
 
+		// api key = 6b356c47-0c9e-45d4-840b-f28b45f1cc28
+		// api secret = 98566c4d-bc3d-4485-9b64-9d674473eb46
+		// cliend id = 7eaf4e7b-b876-48ff-ae89-944369e1740a
+		// client secret = fc3d9282-ef06-4f68-83d9-ef18af8727c9
+		// Authorization = Basic N2VhZjRlN2ItYjg3Ni00OGZmLWFlODktOTQ0MzY5ZTE3NDBhOmZjM2Q5MjgyLWVmMDYtNGY2OC04M2Q5LWVmMThhZjg3MjdjOQ==
+
 		$page = '/home/index';
 		page($page);
+	}
+	private function getToken()
+	{
+		$url        = '/api/oauth/token';
+        $output     = $this->guzzle->request('POST', self::$main_url . $url, [
+            'verify'        => false,
+            'headers'       => [
+                'Authorization'     => 'Basic ' . base64_encode(self::$client_id . ':' . self::$client_secret),
+                'Content-Type'      => 'application/x-www-form-urlencoded'
+            ],
+            'form_params'   => [
+                'grant_type'        => 'client_credentials'
+            ]
+        ]);
+        $output     = json_decode($output->getBody()->getContents(), true);
+        return self::$access_token = $output['access_token'];
 	}
 	
 
--- application/views/page/home/index.php
+++ application/views/page/home/index.php
@@ -19,6 +19,141 @@
         </div>
         <!-- /Title -->
 
+        <!-- INFORMASI SALDO -->
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="panel panel-default card-view pa-0">
+                    <div class="panel-wrapper collapse in">
+                        <div class="panel-body pa-0">
+                            <div class="sm-data-box bg-red">
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-xs-6 text-center pl-0 pr-0 data-wrap-left">
+                                            <span class="weight-500 uppercase-font txt-light block font-13">Saldo</span>
+                                            <span class="txt-light block counter">Rp 118.849.999,53</span>
+                                        </div>
+                                        <div class="col-xs-6 text-center  pl-0 pr-0 data-wrap-right">
+                                            <i class="zmdi zmdi-male-female txt-light data-right-rep-icon"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="panel panel-default card-view pa-0">
+                    <div class="panel-wrapper collapse in">
+                        <div class="panel-body pa-0">
+                            <div class="sm-data-box bg-yellow">
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-xs-6 text-center pl-0 pr-0 data-wrap-left">
+                                            <span class="weight-500 uppercase-font txt-light block">Saldo Masuk bulan
+                                                ini</span>
+                                            <span class="txt-light block counter">Rp 80.746.100,00</span>
+                                        </div>
+                                        <div class="col-xs-6 text-center  pl-0 pr-0 data-wrap-right">
+                                            <i class="zmdi zmdi-redo txt-light data-right-rep-icon"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /INFORMASI SALDO -->
+
+        <!-- CEK MUTASI REKENING -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="panel panel-default card-view">
+                    <div class="panel-heading">
+                        <div class="pull-left">
+                            <h6 class="panel-title txt-dark">Cek Mutasi Rekening</h6>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="panel-wrapper collapse in">
+                        <div class="panel-body">
+                            <div class="form-wrap">
+                                <form class="form-inline" action="" method="GET">
+                                    <div class="form-group mr-15">
+                                        <label class="control-label mr-10" for="start_date">Tanggal Awal</label>
+                                        <input type="date" class="form-control" id="start_date" name="startDate"
+                                            required="">
+                                    </div>
+                                    <div class="form-group mr-15">
+                                        <label class="control-label mr-10" for="end_date">Tanggal Akhir</label>
+                                        <input type="date" class="form-control" id="end_date" name="endDate"
+                                            required="">
+                                    </div>
+                                    <button type="submit" class="btn btn-success btn-anim">
+                                        <i class="icon-rocket"></i><span class="btn-text">Cari</span>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /CEK MUTASI REKENING -->
+        <!-- DATA MUTASI REKENING -->
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="panel panel-default card-view panel-refresh">
+                    <div class="refresh-container">
+                        <div class="la-anim-1"></div>
+                    </div>
+                    <div class="panel-heading">
+                        <div class="pull-left">
+                            <h6 class="panel-title txt-dark">Data Mutasi Rekening</h6>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="panel-wrapper collapse in">
+                        <div class="panel-body row pa-0">
+                            <div class="table-wrap">
+                                <div class="table-responsive">
+                                    <table class="table table-hover mb-0">
+                                        <thead>
+                                            <tr>
+                                                <th>Tanggal</th>
+                                                <th>Branch Code</th>
+                                                <th>Tipe Transaksi</th>
+                                                <th>Total</th>
+                                                <th>Nama Transaksi</th>
+                                                <th>Trailer</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td><span class="txt-dark weight-500">29/08</span></td>
+                                                <td>0000</td>
+                                                <td>D</td>
+                                                <td>
+                                                    <span class="txt-dark weight-500">Rp 9.000.000,00</span>
+                                                </td>
+                                                <td>
+                                                    TRSF E-BANKING DB </td>
+                                                <td>
+                                                    2808/ACDFT/WS950519000000.00 REK KORAN DARI GIRO KE TAPRES </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /DATA MUTASI REKENING -->
+
         <!-- Footer -->
         <footer class="footer container-fluid pl-30 pr-30">
             <div class="row">
@@ -27,7 +162,8 @@
                 </div>
             </div>
         </footer>
-        <!-- /Footer -->
+        <!-- /Footer -
+->
     </div>
 </div>
 <!-- /Main Content -->

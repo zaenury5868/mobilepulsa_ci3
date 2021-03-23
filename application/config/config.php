8284323777b9b5c6e68bb0e7df5baac44abee7a2
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2021-03-23 11:27:34 +0700

    make it better

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set('Asia/Jakarta');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://mobilepulsa.herokuapp.com/';
+$config['base_url'] = 'https://bcamobilepulsa.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
